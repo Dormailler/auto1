57d834a7764f68fa2edbd0243c60e361fdec17f1
Dormailler <101726788+Dormailler@users.noreply.github.com>
2022-09-27 16:27:58 +0900

    Update bitcoinAutoTrade.py

--- bitcoinAutoTrade.py
+++ bitcoinAutoTrade.py
@@ -46,7 +46,7 @@ while True:
         if start_time < now < end_time - datetime.timedelta(seconds=10):
             target_price = get_target_price("KRW-BTC", 0.5)
             current_price = get_current_price("KRW-BTC")
-            if target_price < current_price and current_price<target_price*1.001:
+            if target_price < current_price < target_price*1.001:
                 krw = get_balance("KRW")
                 if krw > 5000:
                     upbit.buy_market_order("KRW-BTC", krw*0.9995)
